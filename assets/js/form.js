76950bb96e062ed0483ff02a2a07cf877e52c135
stgofaz <72179795+stgofaz@users.noreply.github.com>
2024-11-29 21:38:08 -0300

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app); // Initialize Firestore
 
 // Add an event listener to the "Send Message" button
 document.getElementById("sendMessageButton").addEventListener("click", async function (event) {
